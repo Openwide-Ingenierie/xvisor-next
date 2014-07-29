9e0440ad534b2a479ce003717a607d3510427e3c
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2014-07-29 17:31:30 +0200

    [ARM] Improving i.MX clock management.
    The root clocks now read their fixed rate from the device tree.
    This allows to update the rate of all the clocks in the whole clock tree.
    A function to get a clock from its name have also been added.

--- arch/arm/board/common/imx/clk-imx6q.c
+++ arch/arm/board/common/imx/clk-imx6q.c
@@ -74,43 +74,6 @@ static const char *lvds_sels[] = {
 	"pcie_ref", "sata_ref",
 };
 
-enum mx6q_clks {
-	dummy, ckil, ckih, osc, pll2_pfd0_352m, pll2_pfd1_594m, pll2_pfd2_396m,
-	pll3_pfd0_720m, pll3_pfd1_540m, pll3_pfd2_508m, pll3_pfd3_454m,
-	pll2_198m, pll3_120m, pll3_80m, pll3_60m, twd, step, pll1_sw,
-	periph_pre, periph2_pre, periph_clk2_sel, periph2_clk2_sel, axi_sel,
-	esai_sel, asrc_sel, spdif_sel, gpu2d_axi, gpu3d_axi, gpu2d_core_sel,
-	gpu3d_core_sel, gpu3d_shader_sel, ipu1_sel, ipu2_sel, ldb_di0_sel,
-	ldb_di1_sel, ipu1_di0_pre_sel, ipu1_di1_pre_sel, ipu2_di0_pre_sel,
-	ipu2_di1_pre_sel, ipu1_di0_sel, ipu1_di1_sel, ipu2_di0_sel,
-	ipu2_di1_sel, hsi_tx_sel, pcie_axi_sel, ssi1_sel, ssi2_sel, ssi3_sel,
-	usdhc1_sel, usdhc2_sel, usdhc3_sel, usdhc4_sel, enfc_sel, emi_sel,
-	emi_slow_sel, vdo_axi_sel, vpu_axi_sel, cko1_sel, periph, periph2,
-	periph_clk2, periph2_clk2, ipg, ipg_per, esai_pred, esai_podf,
-	asrc_pred, asrc_podf, spdif_pred, spdif_podf, can_root, ecspi_root,
-	gpu2d_core_podf, gpu3d_core_podf, gpu3d_shader, ipu1_podf, ipu2_podf,
-	ldb_di0_podf, ldb_di1_podf, ipu1_di0_pre, ipu1_di1_pre, ipu2_di0_pre,
-	ipu2_di1_pre, hsi_tx_podf, ssi1_pred, ssi1_podf, ssi2_pred, ssi2_podf,
-	ssi3_pred, ssi3_podf, uart_serial_podf, usdhc1_podf, usdhc2_podf,
-	usdhc3_podf, usdhc4_podf, enfc_pred, enfc_podf, emi_podf,
-	emi_slow_podf, vpu_axi_podf, cko1_podf, axi, mmdc_ch0_axi_podf,
-	mmdc_ch1_axi_podf, arm, ahb, apbh_dma, asrc, can1_ipg, can1_serial,
-	can2_ipg, can2_serial, ecspi1, ecspi2, ecspi3, ecspi4, ecspi5, enet,
-	esai, gpt_ipg, gpt_ipg_per, gpu2d_core, gpu3d_core, hdmi_iahb,
-	hdmi_isfr, i2c1, i2c2, i2c3, iim, enfc, ipu1, ipu1_di0, ipu1_di1, ipu2,
-	ipu2_di0, ldb_di0, ldb_di1, ipu2_di1, hsi_tx, mlb, mmdc_ch0_axi,
-	mmdc_ch1_axi, ocram, openvg_axi, pcie_axi, pwm1, pwm2, pwm3, pwm4, per1_bch,
-	gpmi_bch_apb, gpmi_bch, gpmi_io, gpmi_apb, sata, sdma, spba, ssi1,
-	ssi2, ssi3, uart_ipg, uart_serial, usboh3, usdhc1, usdhc2, usdhc3,
-	usdhc4, vdo_axi, vpu_axi, cko1, pll1_sys, pll2_bus, pll3_usb_otg,
-	pll4_audio, pll5_video, pll8_mlb, pll7_usb_host, pll6_enet, ssi1_ipg,
-	ssi2_ipg, ssi3_ipg, rom, usbphy1, usbphy2, ldb_di0_div_3_5, ldb_di1_div_3_5,
-	sata_ref, sata_ref_100m, pcie_ref, pcie_ref_125m, enet_ref, usbphy1_gate,
-	usbphy2_gate, pll4_post_div, pll5_post_div, pll5_video_div, eim_slow,
-	spdif, cko2_sel, cko2_podf, cko2, cko, vdoa, pll4_audio_div,
-	lvds1_sel, lvds2_sel, lvds1_gate, lvds2_gate, clk_max
-};
-
 static struct clk *clk[clk_max];
 static struct clk_onecell_data clk_data;
 
@@ -144,6 +107,7 @@ static struct clk_div_table video_div_table[] = {
 
 static void __init imx6q_clocks_init(struct device_node *ccm_node)
 {
+	u32 rate = 0;
 	struct device_node *np;
 	virtual_addr_t vbase = 0;
 	void __iomem *base;
@@ -151,9 +115,21 @@ static void __init imx6q_clocks_init(struct device_node *ccm_node)
 	int ret;
 
 	clk[dummy] = imx_clk_fixed("dummy", 0);
-	clk[ckil] = imx_obtain_fixed_clock("ckil", 0);
-	clk[ckih] = imx_obtain_fixed_clock("ckih1", 0);
-	clk[osc] = imx_obtain_fixed_clock("osc", 0);
+
+	rate = 0;
+	if (NULL != (np = vmm_devtree_getnode("/clocks/ckil")))
+		vmm_devtree_clock_frequency(np, &rate);
+	clk[ckil] = imx_obtain_fixed_clock("ckil", rate);
+
+	rate = 0;
+	if (NULL != (np = vmm_devtree_getnode("/clocks/ckih1")))
+		vmm_devtree_clock_frequency(np, &rate);
+	clk[ckih] = imx_obtain_fixed_clock("ckih1", rate);
+
+	rate = 0;
+	if (NULL != (np = vmm_devtree_getnode("/clocks/osc")))
+		vmm_devtree_clock_frequency(np, &rate);
+	clk[osc] = imx_obtain_fixed_clock("osc", rate);
 
 	if (NULL == (np = vmm_devtree_find_compatible(NULL, NULL,
 						      "fsl,imx6q-anatop")))
@@ -519,3 +495,8 @@ static void __init imx6q_clocks_init(struct device_node *ccm_node)
 	}
 }
 CLK_OF_DECLARE(imx6q, "fsl,imx6q-ccm", imx6q_clocks_init);
+
+struct clk* imx_clk_get(enum mx6q_clks clkid)
+{
+	return clk[clkid];
+}
