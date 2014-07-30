8909b1a46f41d36c06d1a0a57f03406214db4ee0
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2014-07-30 09:08:56 +0200

    [COMMANDS] Adding imx6 commands set, with some clock API function ports.
    Two imx6 commands have been added.
    imx6 clocks which list all the i.MX clocks with their counters, rate and
    accuracy (the latter is not used).
    imx6 uart <port> which dumps some information about a specified i.MX UART port.

--- drivers/include/drv/clk.h
+++ drivers/include/drv/clk.h
@@ -180,5 +180,7 @@ struct clk *of_clk_get_by_name(struct vmm_devtree_node *np, const char *name)
 }
 #endif
 
+int clk_dump(struct vmm_chardev *cdev);
+int clk_summary_show(struct vmm_chardev *cdev);
 const char *__clk_get_name(struct clk *clk);
 #endif
