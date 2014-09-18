b362a381dbce2161cd57a7bd0fcdad54b7074e27
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2014-09-18 10:00:06 +0200

    [DRIVERS] Add clock name retrieving method.

--- drivers/include/drv/clk.h
+++ drivers/include/drv/clk.h
@@ -182,4 +182,5 @@ struct clk *of_clk_get_by_name(struct vmm_devtree_node *np, const char *name)
 
 int clk_dump(struct vmm_chardev *cdev);
 int clk_summary_show(struct vmm_chardev *cdev);
+const char *__clk_get_name(struct clk *clk);
 #endif
